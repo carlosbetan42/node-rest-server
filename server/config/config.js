022f30e94eff623e9f3a35bd2966daa5167cc05b
Carlos Betancourt <carlos.betancourt@jalisco.gob.mx>
2020-07-09 11:04:06 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -19,5 +19,6 @@ if (process.env.NODE_ENV === 'dev') {
   urlDB = 'mongodb://localhost:27017/cafe';
 } else {
   urlDB = 'mongodb+srv://root:root@cluster0-3kfzc.mongodb.net/cafe';
+  urlDB = process.env.MONGO_URI;
 }
 process.env.URL_DB = urlDB;
